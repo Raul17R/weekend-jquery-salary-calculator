c16a47dc453d644b8506f68278c7f5c0dff802fa
Raul Roldan <raul.ji@outlook.com>
2022-07-16 21:30:38 -0500

    appended information to the dom

--- scripts/client.js
+++ scripts/client.js
@@ -2,6 +2,20 @@ $(readyNow);
 
 function readyNow() {
     console.log('Ready now!');
-    
-    
+    $('#submit').on('click',pushInfo);
+}
+
+function pushInfo() {
+    console.log('In the button');
+    let name = $('#name').val();
+    let lastName = $('#lastName').val();
+    let idNumber = $('#idNumber').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+    //let songName = $('#song-name').val();//value
+    console.log(name,lastName,idNumber,jobTitle,annualSalary);
+
+    $('input').val('');
+
+
 }
